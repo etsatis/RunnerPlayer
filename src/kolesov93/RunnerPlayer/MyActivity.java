525fedd8cd35489796393ea5ece29ac260b90cb4
kolesov93 <kolesov93@gmail.com>
2013-12-29 00:51:44 +0300

    Initial commit

---
+++ src/kolesov93/RunnerPlayer/MyActivity.java
@@ -0,0 +1,15 @@
+package kolesov93.RunnerPlayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
